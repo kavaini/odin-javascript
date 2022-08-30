e612c52771c4af3d8df4d88ffbf5830b2a7119f9
kavaini <karolina.vainilkaite@gmail.com>
2022-08-30 19:44:55 +0300

    feat(game): add game conditions

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
-function getComputerChoice(){
-    return choices[Math.floor(Math.random())* choises.length];
+function getComputerChoice() {
+  return choices[Math.floor(Math.random()) * choices.length];
 }
 
-function playRound(computerSelection, playerSelection){
-    console.log({computerSelection});
-    console.log({playerSelection});
-}
+function playRound(computerSelection, playerSelection) {
+  console.log({ computerSelection });
+  console.log({ playerSelection });
 
-if (computerSelection === playerSelection){
+  if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats Paper";
+  } else computerSelection === playerSelection;
+  {
     return "It's a draw";
-}else if (playerSelection === "Paper" && computerSelection === "Rock"){
-    return "You Win! Paper beats Rock"
-}else if (playerSelection === "Paper" && computerSelection === "Scissors"){
-    return "You Lose! Scissors beats Paper"
-}else if (playerSelection === "Rock" && computerSelection === "Paper"){
-    return "You Lose! Paper beats Rock"
-}else if(playerSelection === "Rock" && computerSelection === "Scissors"){
-    return "You Win! Rock beats Scissors"
-}else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-    return "You Win! Scissors beats Paper"
-}else (playerSelection === "Scissors" && computerSelection === "Rock"){
-    return "You Lose! Rock beats Scissors"
+  }
 }
+
+const computerSelection = getComputerChoice();
+console.log(`Computer chose ${computerSelection}`);
+
+const playerSelection = "Paper";
+console.log(`Player chose ${playerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
